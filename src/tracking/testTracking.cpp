91f01dc5e23fc0b655ea2e5e29175314c3c697ad
sgunnam <sridhargunnam@gmail.com>
2020-10-19 20:16:18 -0400

    Contours partially working after teeking parameters, and applying constraints on contour area.

--- src/tracking/testTracking.cpp
+++ src/tracking/testTracking.cpp
@@ -3,9 +3,10 @@
 //
 #include "tracking.h"
 // TODO
-// Morph the bg-sub mask image to get countors
-// Then define state space - get centroid of countour, velocity, orientation(may need different tracking, measurement model)
+// Morph the bg-sub mask image to get contours
+// Then define state space - get centroid of contour, velocity, orientation(may need different tracking, measurement model)
 
+// TODO Oct 19th: Remove the bg, and then just use canny + threshold and then run the contours, apple to apple with squares.cpp
 //Assumptions
 /*
  * 1) Only foreground moving
@@ -25,6 +26,43 @@ const int h = color_frame.as<rs2::video_frame>().get_height();
 image = cv::Mat(cv::Size(w, h), CV_8UC3, (void *)color_frame.get_data(), cv::Mat::AUTO_STEP);
 }
 
+//void morphAndGetCountourImage(cv::Mat& im0_foreground_mask){
+//  {
+//    cv::Mat im0gray;
+//    cv::cvtColor(im0, im0gray, cv::COLOR_BGR2GRAY);
+//    cv::Mat im0blur;
+//    int gaussian_kernel_width = 25;
+//    cv::Size gaussian_kernel = cv::Size(gaussian_kernel_width, gaussian_kernel_width);
+//    cv::GaussianBlur(im0gray, im0blur, gaussian_kernel, 0, 0);
+//
+//
+//    cv::erode(im0blur, im0blur, cv::Mat() , cv::Point (-1, -1),5 , 1, 1);
+//    cv::dilate(im0blur, im0blur, cv::Mat(), cv::Point(-1, -1), 5, 1, 1);
+//
+//    cv::Mat im0edge;
+//    double th0 = 5;
+//    double th1 = 30;
+//
+//    Canny(im0blur, im0edge, th0, th1, 3, false);
+//
+//    std::vector<std::vector<cv::Point>> im0Contours;
+//    cv::findContours(im0_foreground_mask, im0Contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+//    cv::drawContours(im0, im0Contours, -1, cv::Scalar(0, 0, 255), 3);
+//  }
+//}
+
+void MyMorph(cv::Mat im){
+  int gaussian_kernel_width = 25;
+  cv::Size gaussian_kernel = cv::Size(gaussian_kernel_width, gaussian_kernel_width);
+  cv::GaussianBlur(im, im, gaussian_kernel, 0, 0);
+  int thresh = 50;
+  //cv::Mat imCanny;
+  //Canny(im, imCanny, 0, thresh, 5);
+  //im = imCanny;
+  //cv::erode(im, im, cv::Mat() , cv::Point (-1, -1),5 , 1, 1);
+  cv::dilate(im, im, cv::Mat(), cv::Point(-1, -1), 5, 1, 1);
+  //cv::threshold(im, im, 127, 255, cv::THRESH_BINARY);
+}
 
 int main()
 {
@@ -36,22 +74,20 @@ int main()
   rs2::pipeline pipe;
   // Start streaming with default recommended configuration
   pipe.start();
-  int i = 0;
 
   cv::namedWindow("Background substraction", cv::WINDOW_AUTOSIZE);
 
-    using namespace cv;
-    using namespace std;
-
   {
     //create Background Subtractor objects
-    Ptr<BackgroundSubtractor> pBackSub;
-      pBackSub = createBackgroundSubtractorMOG2();
-      //pBackSub = createBackgroundSubtractorKNN();
+    cv::Ptr<cv::BackgroundSubtractor> pBackSub;
+//      pBackSub = cv::createBackgroundSubtractorMOG2();
+      pBackSub = cv::createBackgroundSubtractorKNN();
 
-    Mat frame, fgMask;
+    cv::Mat frame, fgMask;
     while (true) {
       getCurrentFrame( pipe,  color_map, frame);
+      cv::Mat frame_orig = frame.clone();
+      MyMorph(frame);
       //capture >> frame;
       if (frame.empty())
         break;
@@ -60,54 +96,69 @@ int main()
       //get the frame number and write it on the current frame
       rectangle(frame, cv::Point(10, 2), cv::Point(100,20),
                 cv::Scalar(255,255,255), -1);
-      //stringstream ss;
-      //ss << capture.get(CAP_PROP_POS_FRAMES);
-      //string frameNumberString = ss.str();
-//      putText(frame, frameNumberString.c_str(), cv::Point(15, 15),
-//              FONT_HERSHEY_SIMPLEX, 0.5 , cv::Scalar(0,0,0));
+
+      // Countours
+      std::vector<std::vector<cv::Point>> im0Contours;
+      cv::findContours(fgMask, im0Contours, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE);
+      //cv::findContours(frame, im0Contours, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE);
+
+      std::vector<std::vector<cv::Point>> fg_contour;
+      std::vector<cv::Point> approx;
+      for(auto i=0; i< im0Contours.size(); i++) {
+        approxPolyDP(im0Contours[i], approx, arcLength(im0Contours[i], true) * 0.02, true);
+        if(fabs(contourArea(approx)) > 5000)
+        {
+          fg_contour.push_back(im0Contours[i]);
+        }
+      }
+      cv::drawContours(frame_orig, fg_contour, -1, cv::Scalar(0, 0, 255), 3);
+
       //show the current frame and the fg masks
       imshow("Frame", frame);
       imshow("FG Mask", fgMask);
+      imshow("Countours", frame_orig);
+
       //get the input from the keyboard
-      int keyboard = waitKey(30);
+      int keyboard = cv::waitKey(30);
       if (keyboard == 'q' || keyboard == 27)
         break;
     }
     }
 
     // Optical flow - LK features tracking
+    /*
     {
       // Create some random colors
-      vector<Scalar> colors;
-      RNG rng;
+      std::vector<cv::Scalar> colors;
+      cv::RNG rng;
       for (int i = 0; i < 100; i++) {
         int r = rng.uniform(0, 256);
         int g = rng.uniform(0, 256);
         int b = rng.uniform(0, 256);
-        colors.push_back(Scalar(r, g, b));
+        colors.push_back(cv::Scalar(r, g, b));
       }
-      Mat old_frame, old_gray;
-      vector<Point2f> p0, p1;
+      cv::Mat old_frame, old_gray;
+      std::vector<cv::Point2f> p0, p1;
       // Take first frame and find corners in it
       getCurrentFrame( pipe,  color_map, old_frame);
       //capture >> old_frame;
-      cvtColor(old_frame, old_gray, COLOR_BGR2GRAY);
-      goodFeaturesToTrack(old_gray, p0, 100, 0.3, 7, Mat(), 7, false, 0.04);
+      cvtColor(old_frame, old_gray, cv::COLOR_BGR2GRAY);
+      goodFeaturesToTrack(old_gray, p0, 100, 0.3, 7, cv::Mat(), 7, false, 0.04);
       // Create a mask image for drawing purposes
-      Mat mask = Mat::zeros(old_frame.size(), old_frame.type());
+      cv::Mat mask = cv::Mat::zeros(old_frame.size(), old_frame.type());
       while (true) {
-        Mat frame, frame_gray;
+        cv::Mat frame, frame_gray;
         getCurrentFrame( pipe,  color_map, frame);
         //capture >> frame;
         if (frame.empty())
           break;
-        cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
+        cvtColor(frame, frame_gray, cv::COLOR_BGR2GRAY);
         // calculate optical flow
-        vector<uchar> status;
-        vector<float> err;
-        TermCriteria criteria = TermCriteria((TermCriteria::COUNT) + (TermCriteria::EPS), 10, 0.03);
-        calcOpticalFlowPyrLK(old_gray, frame_gray, p0, p1, status, err, Size(15, 15), 2, criteria);
-        vector<Point2f> good_new;
+        std::vector<uchar> status;
+        std::vector<float> err;
+        cv::TermCriteria criteria = cv::TermCriteria((cv::TermCriteria::COUNT) + (cv::TermCriteria::EPS), 10, 0.03);
+        calcOpticalFlowPyrLK(old_gray, frame_gray, p0, p1, status, err, cv::Size(15, 15), 2, criteria);
+        std::vector<cv::Point2f> good_new;
         for (uint i = 0; i < p0.size(); i++) {
           // Select good points
           if (status[i] == 1) {
@@ -117,10 +168,10 @@ int main()
             circle(frame, p1[i], 5, colors[i], -1);
           }
         }
-        Mat img;
+        cv::Mat img;
         add(frame, mask, img);
         imshow("Frame", img);
-        int keyboard = waitKey(30);
+        int keyboard = cv::waitKey(30);
         if (keyboard == 'q' || keyboard == 27)
           break;
         // Now update the previous frame and previous points
@@ -128,8 +179,7 @@ int main()
         p0 = good_new;
       }
     }
-
-
+*/
 
   // Our initial approach - morphology based, didn't work yet
   /*
@@ -194,7 +244,7 @@ int main()
     //  cv::threshold(im1gray, im1Thres, 127, 255, 0);
 
     cv::erode(im0blur, im0blur, cv::Mat() , cv::Point (-1, -1),5 , 1, 1);
-    cv::dilate(im1blur, im0blur, cv::Mat(), cv::Point(-1, -1), 5, 1, 1);
+    cv::dilate(im0blur, im0blur, cv::Mat(), cv::Point(-1, -1), 5, 1, 1);
     cv::imshow("erode", im0blur);
     waitKey(30000);
     cv::Mat im0edge, im1edge;
